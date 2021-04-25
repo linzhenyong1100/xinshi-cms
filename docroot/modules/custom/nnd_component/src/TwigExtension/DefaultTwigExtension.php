717a881ec5976f045eec9feca876be19f144f197
donkblongsi <donkblongsi@yeah.net>
2021-04-25 23:30:32 +0800

    add method to twig

--- docroot/modules/custom/nnd_component/src/TwigExtension/DefaultTwigExtension.php
+++ docroot/modules/custom/nnd_component/src/TwigExtension/DefaultTwigExtension.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\nnd_component\TwigExtension;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Url;
 use Drupal\nnd_component\CommonUtil;
 use Drupal\taxonomy\Entity\Term;
@@ -27,7 +28,18 @@ class DefaultTwigExtension extends \Twig_Extension {
         'removeBaseUrl',
         [$this, 'removeBaseUrl']
       ),
-
+      new \Twig_SimpleFunction(
+        'imageStyle',
+        [$this, 'getImageStyle']
+      ),
+      new \Twig_SimpleFunction(
+        'timestampFormat',
+        [$this, 'timestampFormat']
+      ),
+      new \Twig_SimpleFunction(
+        'entityUrl',
+        [$this, 'entityUrl']
+      ),
     ];
   }
 
@@ -36,8 +48,32 @@ class DefaultTwigExtension extends \Twig_Extension {
    * @param $url
    * @return bool|string
    */
-  public static function removeBaseUrl($url) {
+  public function removeBaseUrl($url) {
     return CommonUtil::removeBaseUrl($url);
+  }
+
+  /**
+   * @param $fid
+   * @param string $style
+   * @param bool $remove_base_url
+   * @return \Drupal\Core\GeneratedUrl|string
+   */
+  public function getImageStyle($fid, $style = 'crop', $remove_base_url = TRUE) {
+    return CommonUtil::getImageStyle($fid, $style, $remove_base_url);
+  }
+
+  /**
+   * Return date string.
+   * @param $timestamp
+   * @param string $format
+   * @return false|string
+   */
+  public function timestampFormat($timestamp, $format = 'Y-m-d H:i:s') {
+    return date($format, $timestamp);
+  }
+
+  public function entityUrl(EntityInterface $entity) {
+    return $entity->toUrl()->toString();
   }
 
 }
